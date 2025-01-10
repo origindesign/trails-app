ea4fd4e1334d53d27eeb36bbefa3789c2805f5c6
Sam Medhurst <smedhurst@originoutside.com>
2025-01-10 12:58:46 -0800

    structure data files

---
+++ src/data/parking.js
@@ -0,0 +1,15 @@
+// markerData.js
+const parkingData = [
+    {
+        latLng: [50.366123904300906, -119.10359085473954],
+        popupContent:
+            "<h3 class='t-c-teal'>Parking</h3><a class='link--parking' href=''>Directions</a>",
+    },
+    {
+        latLng: [50.25274347498934, -119.26611525070177],
+        popupContent:
+            "<h3 class='t-c-teal'>Parking</h3><a class='link--parking' href=''>Directions</a>",
+    },
+];
+
+export default parkingData;
